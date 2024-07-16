bda9e9480ab8ba6df900b2a9b2989f8029de91ff
akash <akash.deokar@enreap.com>
2024-07-16 15:17:57 +0530

    update111

--- Downloads/akash_Deokar/practice.py
+++ Downloads/akash_Deokar/practice.py
@@ -6,3 +6,5 @@ print("Akash Ravindra Deokar")
 print('prasad pawar')
 print ("hello")
 print("hiii")
+print ("""Invitation links automatically expire after 30 days, but
+       an org admin can also turn them off before the 30 days are up. They can also choose to generate a new URL.""")
