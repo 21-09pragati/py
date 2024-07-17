86e7f210577b3734702051c11b8705b0daad9450
akash <akash.deokar@enreap.com>
2024-07-17 19:14:15 +0530

    update111

--- Downloads/akash_Deokar/practice.py
+++ Downloads/akash_Deokar/practice.py
@@ -1,10 +1,14 @@
-list=[1,2,3,4,5,6,7]
-print(list)
+import requests
+r= requests.get('https://www.espncricinfo.com/')
+print(r)
+print(r.content)
 
 
-print("Akash Ravindra Deokar")
-print('prasad pawar')
-print ("hello")
-print("hiii")
-print ("""Invitation links automatically expire after 30 days, but
-       an org admin can also turn them off before the 30 days are up. They can also choose to generate a new URL.""")
+
+response= requests.get ('https://www.espncricinfo.com/')
+
+print(response.status_code)
+
+#Authentication using Python Requests
+
+print(response.headers)
