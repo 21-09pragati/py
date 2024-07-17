dc2db940e7d1525c8a2d84ea2cdfdbfcec7c7a3a
pragati <pragati.patil@enreap.com>
2024-07-17 19:11:06 +0530

    Added 16

---
+++ Downloads/py/Practice.py
@@ -0,0 +1,44 @@
+#16/7/24
+
+
+
+'''
+#VARIABLES
+
+name="Pragati"
+print(name)
+
+place=input("Enter Place name: ")
+print(place)
+
+#DATA TYPES
+
+a=12
+b=1.2
+c="Pragati"
+d=True
+print("Type of a: ",type(a))
+print("Type of b:",type(b))
+print("Type of c:",type(c))
+print("Type of d:",type(d))
+
+#CONCATE VARIABLE IN STRING
+year=2023
+name="Pragati"
+university="Shivaji University"
+
+st1=name + ' is passout in '+ str(year) + ' from ' + university + ' ' 
+print(st1) '''
+
+#TYPECASTING
+
+p=int(1.2)
+q=float(11)
+r=str(12)
+ss=float("99")
+s=float("23.4")
+t=str(22)
+u=str(22.4)
+v=int("3")
+
+print([p,q,r,s,t,u,v])
