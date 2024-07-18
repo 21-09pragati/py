f4f31eacf6a33696dcc505c3f18b1d3a975d7031
akash <akash.deokar@enreap.com>
2024-07-18 19:02:25 +0530

    update111

--- Downloads/akash_Deokar/practice.py
+++ Downloads/akash_Deokar/practice.py
@@ -1,14 +1,33 @@
 import requests
-r= requests.get('https://www.espncricinfo.com/')
-print(r)
-print(r.content)
+#r= requests.get('https://www.espncricinfo.com/')
+#print(r)
+#print(r.content)
 
 
 
-response= requests.get ('https://www.espncricinfo.com/')
+#response= requests.get ('https://www.espncricinfo.com/')
 
-print(response.status_code)
+#print(response.status_code)
 
 #Authentication using Python Requests
 
-print(response.headers)
+#print(response.headers)
+
+#numpy :-
+
+
+import numpy as np
+#2D:-
+list=np.array([[2,3],[4,5]])
+print(list)
+#indexing:-
+print(list[0])
+
+print(list[0][1])
+
+list1 = np.array ([[1,11,111],[2,22,222]])
+print(list1)
+print("##########################")
+print(list1[1])
+print("##########################")
+print(list1[1][-1])
