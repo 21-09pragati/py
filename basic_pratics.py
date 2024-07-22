f611e24a529d52202a81a0c57a06947369d192c5
pooja <pooja.nimbalkar@enreap.com>
2024-07-22 19:08:54 +0530

    Adding new file to poojak

---
+++ basic_pratics.py
@@ -0,0 +1,56 @@
+#input("what is your name  ")
+
+#print("hey"+" "+input("What is your name ")+" "+"how are you")
+
+'''name= input('what is your name : ')
+print(len(name))'''
+
+
+
+'''Swaping two numbers
+
+
+a=input('Enter value of a : ')
+b=input('Enter value of b : ')
+c=a
+a=b
+b=c
+print('a = '+ a)
+print('b = '+b)            ...'''
+
+
+
+''' Swaping two numbers without using third numbers.
+
+
+a=int(input('Enter value of a : '))
+b=int(input('Enter value of b : '))
+a=a+b
+b=a-b
+a=a-b
+print('a = ', a)
+print('b = ', b) '''
+
+# Sum of two numbers.
+
+'''X=int(input("Enter value of X : "))
+Y=int(input("Enter value of Y : "))
+sum=X+Y
+print("Sum of X and Y is : ",sum)'''
+
+
+#sum of two digit of numbers
+'''
+number = input("Enter a two digit number:")
+print(type(number))
+sum=int(number[0])+int(number[1])
+print(sum)'''
+
+
+
+#Write a program to calculate BMI.
+
+weight=int(input("Enter Weight in kg:"))
+height=float(input("Enter a height in mtr:"))
+BMI=weight/(height*height)
+print("BMI of enter values is :",BMI)
